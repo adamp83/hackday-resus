770db67d72f709dccf007a5b81f29e6b6c2e116c
Adam Pennycuick <adampennycuick@gmail.com>
2015-05-16 15:03:02 +0100

    Added timer

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,9 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	
+	resusTimer: 0.0,
+	timerStarted: false,
+	
 	records: function(){
 		return this.store.all('record');
 	}.property('record'),
+	
+	setupTimer: function(){
+		var _this = this;
+		setInterval(function(){
+			if(_this.get('timerStarted')){
+				_this.set('resusTimer', Number((_this.get('resusTimer') + 0.1).toFixed(2)));
+			}
+		}, 100);
+	}.on('init'),
 	
 	actions: {
 		addRecord: function(button){
@@ -12,9 +24,25 @@ export default Ember.Controller.extend({
 				button: button,
 				time: Date.now()
 			})
+		},
+		
+		startTimer: function(){
+			this.set('timerStarted', true);
+			this.store.createRecord('record',{
+				time: Date.now(),
+				text: 'Arrest Started'
+			});
+			
+		},
+		
+		stopTimer: function(){
+			this.set('timerStarted', false);
+			this.store.createRecord('record',{
+				time: Date.now(),
+				text: 'Timer stopped'
+			});
 		}
-		
-		
+
 	}
 	
 });
