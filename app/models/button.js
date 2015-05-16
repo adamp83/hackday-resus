83b441b6252d35ea1356dbee1db1b48b34dc0d52
Andrew McArdle <andrew.mcardle@gmail.com>
2015-05-16 17:01:02 +0100

    Rationalised actions

--- app/models/button.js
+++ app/models/button.js
@@ -14,42 +14,40 @@ var Button = DS.Model.extend({
 
 Button.reopenClass({
 	FIXTURES: [
-		{id: 1, title: "Airway status", statuses: ['Patent', 'Non-patent'], , category: 1},
-		{id: 2, title: "Airway support", statuses: ['Manouevres', 'Guedel', 'Nasopharyngeal', 'Laryngeal mask-airway', 'Endotracheal tube', 'Trachoetomy', 'Tracheostomy'], category: 1},
-		{id: 5, title: "Intubation attempt started", category: 1},
-		{id: 6, title: "Intubation attempt ended", category: 1},
+		{id: 1, title: "Airway status", statuses: ['Patent', 'Non-patent'] , category: 1},
+		{id: 2, title: "Airway support", statuses: ['None required', 'Manouevres', 'Guedel', 'Nasopharyngeal', 'Laryngeal mask-airway', 'Endotracheal tube', 'Trachoetomy', 'Tracheostomy'], category: 1},
+		{id: 3, title: "Intubation", hasTimer: true, category: 1},
 		
-		{id: 35, title: "Breathing", statuses: ['Sufficient self-ventilation', 'Insufficient self-ventilation', 'Bag-valve mask ventilation', 'Manual ventilation through ', 'Mechanically ventilated'], category: 2},
-		{id: 36, title: "Administer O2", category: 2},
-		{id: 7, title: "Administer O2", category: 2},
-		{id: 8, title: "Bag-valve mask ventilation", category: 2},
-		{id: 9, title: "Ventilator", category: 2},
+		{id: 4, title: "Breathing", statuses: ['Sufficient self-ventilation', 'Insufficient self-ventilation', 'CPAP', 'Bilevel non-invasive pressure support', 'Bag-valve mask ventilation', 'Manual ventilation', 'Mechanically ventilated'], category: 2},
+		{id: 5, title: "Oxygen delivery", statuses: ['High flow O2', 'Venturi device', 'Nasal cannulae', 'Face-mask', 'Room air'], category: 2},
 		
-		{id: 10, title: "Rhythm", statuses: ['Asystole', 'PEA', 'VF', 'Spontaneous circulation'], status: undefined, category: 3},
-		{id: 14, title: "IV access", category: 3},
-		{id: 15, title: "DC Shock", counted: true, category: 3, hasTimer: true},
-		{id: 16, title: "ROSC", category: 3},
-		{id: 17, title: "Adrenaline", category: 3, hasTimer: true},
-		{id: 18, title: "Amiodarone", category: 3},
-		{id: 19, title: "Fluid bolus", category: 3},
+		{id: 6, title: "Rhythm", statuses: ['Asystole', 'PEA', 'VF', 'Sinus rhythm', 'Arrhythmia'], status: undefined, category: 3},
+		{id: 7, title: "IV access", category: 3},
+		{id: 8, title: "DC Shock", counted: true, category: 3, hasTimer: true},
+		{id: 9, title: "Adrenaline", counted: true, category: 3, hasTimer: true},
+		{id: 10, title: "Amiodarone", counted: true, category: 3},
+		{id: 11, title: "Fluid bolus", counted: true, category: 3},
 		
-		{id: 20, title: "Glucose", category: 4},
-		{id: 21, title: "Blood loss", category: 4},
-		{id: 22, title: "Pupils", category: 4},
-		{id: 23, title: "Temperature", category: 4},
-		
-		{id: 24, title: "Hypoxia", statuses: ['Considered causal', 'Not considered causal'], category: 5},
-		{id: 25, title: "Hypovolaemia", statuses: ['Not suspected', 'Presumed', 'Suspected', 'Confirmed'], category: 5},
-		{id: 26, title: "Electrolyte abnormalities", statuses: ['Awaited', 'Checked - no issues', 'Problems'], category: 5},
-		{id: 27, title: "Hypothermia", statuses: ['None', 'Present'], category: 5},
-		{id: 28, title: "Tension pneumothorax", statuses: ['Not suspected', 'Suspected', 'Confirmed', 'Treated'], category: 5},
-		{id: 29, title: "Tamponade", statuses: ['Not suspected', 'Suspected', 'Confirmed', 'Treated'], category: 5},
-		{id: 30, title: "Thrombosis", statuses: ['Not suspected', 'Suspected', 'Confirmed', 'Treated'], category: 5},
-		{id: 31, title: "Toxins", statuses: ['Not suspected', 'Suspected', 'Confirmed', 'Treated'], category: 5},
-		
-		{id: 32, title: "Member arrives", category: 6},
-		{id: 33, title: "Member leaves", category: 6},
-		{id: 34, title: "Delegate task", category: 6},
+		{id: 12, title: "Glucose", statuses: ['Low', 'Acceptable', 'High'], category: 4},
+		{id: 13, title: "Responsiveness", statuses: ['Alert', 'Verbal', 'Pain', 'Unresponsive'], category: 4},
+		{id: 14, title: "Pupils", category: 4},
+		{id: 15, title: "Motor GCS", statuses: ['6 - Follows commands', '5 - Localises pain', '4 - Withdraws from pain', '3 - Flexion response', '2 - Extensor response', '1 - No response'], category: 4},
+		{id: 16, title: "Verbal GCS", statuses: ['5 - Coherent', '4 - Confused/inappropriate speech', '3 - Inappropriate words', '2 - Noises', '1 - No sounds'], category: 4},
+		{id: 17, title: "Eye GCS", statuses: ['4 - Confused/inappropriate speech', '3 - Inappropriate words', '2 - Noises', '1 - No sounds'], category: 4},
+		{id: 18, title: "Temperature", category: 4},
+
+		{id: 19, title: "Hypoxia", statuses: ['Considered causal', 'Not considered causal'], category: 5},
+		{id: 20, title: "Hypovolaemia", statuses: ['Not suspected', 'Presumed', 'Suspected', 'Confirmed'], category: 5},
+		{id: 21, title: "Electrolyte abnormalities", statuses: ['Awaited', 'Checked - no issues', 'Problems'], category: 5},
+		{id: 22, title: "Hypothermia", statuses: ['None', 'Present'], category: 5},
+		{id: 23, title: "Tension pneumothorax", statuses: ['Not suspected', 'Suspected', 'Confirmed', 'Treated'], category: 5},
+		{id: 24, title: "Tamponade", statuses: ['Not suspected', 'Suspected', 'Confirmed', 'Treated'], category: 5},
+		{id: 25, title: "Thrombosis", statuses: ['Not suspected', 'Suspected', 'Confirmed', 'Treated'], category: 5},
+		{id: 26, title: "Toxins", statuses: ['Not suspected', 'Suspected', 'Confirmed', 'Treated'], category: 5},
+
+		{id: 27, title: "Member arrives", category: 6},
+		{id: 28, title: "Member leaves", category: 6},
+		{id: 29, title: "Delegate task", category: 6},
 		
 		
 	]
