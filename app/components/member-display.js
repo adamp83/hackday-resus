7d5913663ed19ea4f307b92d37d339785afbff4d
Adam Pennycuick <adampennycuick@gmail.com>
2015-05-17 13:49:04 +0100

    Added names to people

--- app/components/member-display.js
+++ app/components/member-display.js
@@ -8,8 +8,21 @@ classNames        : [ 'draggableItem', 'teamMember' ],
   attributeBindings : [ 'draggable' ],
   draggable         : 'true',
 	
+	editName: false,
+	
 	dragStart: function(event){
 		console.log('Start dragging!');
 		return event.dataTransfer.setData('text/data', this.get('member.id'));
+	},
+	
+	actions: {
+		doEditName: function(){
+			this.set('editName', true);
+		},
+		saveEditName: function(){
+			this.set('editName', false);
+		}
 	}
+	
+	
 });
