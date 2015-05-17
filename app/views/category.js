410511f4eb8257d5fa7bc79a0d200c5af3d1c356
Finn Catling <finn.catling@gmail.com>
2015-05-17 09:59:11 +0100

    css tweaks

--- app/views/category.js
+++ app/views/category.js
@@ -2,5 +2,5 @@ import Ember from 'ember';
 
 export default Ember.View.extend({
 	templateName: 'category',
-	classNames: ["col-xs-5ths", "resus-col", "resus-even-col"]
+	classNames: ["col-xs-2", "resus-col"]
 });
